6da7265655e226ebd7155c1b1684becc3e8d242a
xuesen <xuesen@users.noreply.github.com>
2018-07-10 15:21:40 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -8,12 +8,16 @@ secret = conf.get("section", "secret")
 
 #精度和最小挂单量
 btc = {'name': 'btcusdt', 'coin': 'btc', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+eth = {'name': 'ethusdt', 'coin': 'eth', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
 bch = {'name': 'bchusdt', 'coin': 'bch', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
 ltc = {'name': 'ltcusdt', 'coin': 'ltc', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
-eth = {'name': 'ethusdt', 'coin': 'eth', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
 etc = {'name': 'etcusdt', 'coin': 'etc', 'price_precision': 2, 'amount_precision': 4, 'min_amount': 0.001}
+xrp = {'name': 'xrpusdt', 'coin': 'xrp', 'price_precision': 4, 'amount_precision': 1, 'min_amount': 5}
 ft = {'name': 'ftusdt', 'coin': 'ft', 'price_precision': 6, 'amount_precision': 0, 'min_amount': 5}
+zip = {'name': 'zipusdt', 'coin': 'zip', 'price_precision': 6, 'amount_precision': 0, 'min_amount': 5}
+fi = {'name': 'fiusdt', 'coin': 'fi', 'price_precision': 6, 'amount_precision': 0, 'min_amount': 5}
 btm = {'name': 'btmusdt', 'coin': 'btm', 'price_precision': 4, 'amount_precision': 1, 'min_amount': 5}
+bnb = {'name': 'bnbusdt', 'coin': 'bnb', 'price_precision': 4, 'amount_precision': 1, 'min_amount': 5}
 
 #FTUSDT，BTCUSDT,ETHUSDT买单数量不少于10usdt
 
@@ -21,20 +25,28 @@ btm = {'name': 'btmusdt', 'coin': 'btm', 'price_precision': 4, 'amount_precision
 mount  = int(conf.get("section","mount"))         #数量单位USDT
 symbol = ""        #交易对
 trans = conf.get("section","symbol")
-if trans == 'ft':
-    symbol = ft
 if trans == 'btc':
     symbol = btc
+if trans == 'eth':
+    symbol = eth
 if trans == 'bch':
     symbol = bch
 if trans == 'ltc':
     symbol = ltc
-if trans == 'eth':
-    symbol =eth
-if trans == etc:
-    symbol = etc
+if trans == 'etc':
+    symbol =etc
+if trans == 'xrp':
+    symbol = xrp
+if trans == 'ft':
+    symbol = ft
+if trans == 'zip':
+    symbol = zip
+if trans == 'fi':
+    symbol = fi
 if trans == 'btm':
     symbol = btm
+if trans == 'bnb':
+    symbol = bnb
 
 order_sleep = int(conf.get("section","order_sleep"))    #订单处理等待时间s
 
---
+++ readme.txt
@@ -0,0 +1,32 @@
+Key：FCOIN交易平台上你自己申请的API的key值
+secret：API的secret值
+mount USDT单次交易数量
+symbol 交易对，仅提供与usdt的交易对,例如：symbol=ft，就是ftusdt交易对
+order_sleep为订单交易等待时间
+trading_Strategy 交易策略
+
+代码有问题可以加微信说，互相交朋友：jianbingguozi1995
+
+代码提供源码，可以自行检查是否有后门，如果老哥们用的好的话给点饭钱就行，在这里给各位老铁抱拳了
+
+FT地址：0x96e6512d866ecf64f40be499a9cfacaf2c6000b0
+
+后续会慢慢做量化交易，希望能从小白的道路上走向财务自由
+
+作者：一条小猪佩奇
+
+开源倡导者，源码地址https://github.com/xuesen/Fcoin-FT
+
+2018-07-07更新
+trading_Strategy = 1 交易策略为两种
+                     等于1代表平价策略(买一卖一取中间值，特点是最容易成交快速买卖)
+                     等于2代表差价策略买一卖一(低买高卖)
+                     上涨时建议使用平价
+
+
+
+
+
+
+
+
